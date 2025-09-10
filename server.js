df60f402ca0c7aa69e5373ff792f755f9eced676
Muhannadjz <muhannadzowayed05@gmail.com>
2025-09-10 14:19:59 +0300

    debugging

--- server.js
+++ server.js
@@ -7,24 +7,3 @@ const cors = require('cors');
 const propertyRoutes = require('./routes/realEstate');
 const authRoutes = require('./routes/auth');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-
-const mongoURI = process.env.DB_URI;
-
-mongoose.connect(mongoURI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
-
-app.get('/muh', (req, res) => res.json('SUCCESS'));
-
-app.use('/api/properties', propertyRoutes);
-app.use('/api/auth', authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port${PORT}`);
-});
