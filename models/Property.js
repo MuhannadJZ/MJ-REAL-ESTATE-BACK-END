cdb5affb97ddc02a5921e09992a8ba303b0ab3a7
Muhannadjz <muhannadzowayed05@gmail.com>
2025-09-10 14:09:46 +0300

    property model aded

---
+++ models/Property.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const propertySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    default: '',
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',  
+    // required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Property', propertySchema);
