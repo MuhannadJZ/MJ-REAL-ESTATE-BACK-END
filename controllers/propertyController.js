1908f525955f9d92b0709f8da5bdf082b9c4e2ba
Muhannadjz <muhannadzowayed05@gmail.com>
2025-09-10 14:14:51 +0300

    controllers exporting

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -63,3 +63,11 @@ const deleteProperty = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+module.exports = {
+  createProperty,
+  getAllProperties,
+  getPropertyById,
+  updateProperty,
+  deleteProperty,
+};
